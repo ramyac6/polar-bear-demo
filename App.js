89d8492be2713d0513db8c1db22e1ea776ea3e06
Ramya Challa <ramyac6@cs.washington.edu>
2020-08-30 00:05:00 -0700

    more changes

--- App.js
+++ App.js
@@ -3,7 +3,12 @@ import React, { useEffect, useState } from "react";
 import { firebase } from "./src/firebase/config";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { LoginScreen, HomeScreen, RegistrationScreen } from "./src/screens";
+import {
+  LoginScreen,
+  HomeScreen,
+  RegistrationScreen,
+  BearPartScreen,
+} from "./src/screens";
 import { decode, encode } from "base-64";
 if (!global.btoa) {
   global.btoa = encode;
